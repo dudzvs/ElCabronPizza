a8b2ae15b23c7fbe08597881fbf057d9c9ef9639
Eduardo <eduardoviniciusteixeira@gmail.com>
2023-12-18 16:23:21 -0300

    Create nav and style them

--- src/website.js
+++ src/website.js
@@ -9,8 +9,34 @@ function createHeader() {
   restaurantName.textContent = "El Cabron";
 
   header.appendChild(restaurantName);
+  header.appendChild(createNav())
 
   return header;
+}
+
+function createNav() {
+  const nav = document.createElement('nav');
+  const homeBtn = document.createElement('button');
+  homeBtn.classList.add('button-nav');
+  homeBtn.textContent = 'Home';
+
+  homeBtn.onclick = () => loadHome();
+
+  const menuBtn= document.createElement('button');
+  menuBtn.classList.add('button-nav');
+  menuBtn.textContent = 'Menu';
+  //loadMenu()
+
+  const contactBtn = document.createElement('button');
+  contactBtn.classList.add('button-nav');
+  contactBtn.textContent = 'Contact';
+  //loadContact()
+
+  nav.appendChild(homeBtn);
+  nav.appendChild(menuBtn);
+  nav.appendChild(contactBtn);
+
+  return nav;
 }
 
 function initizalizeWebsite() {
